57d19092922ad10c0eb36eb607241180b87dea33
Abra Geiger <114822735+abrageiger7@users.noreply.github.com>
2023-05-22 23:29:04 -0400

    Added documentation and labelled magic variables convolved_pbfs.py

--- convolved_pbfs.py
+++ convolved_pbfs.py
@@ -1,52 +1,65 @@
-#convolved_pbfs.py
-#includes both exponential and pbf broadened profile templates
-#Last edited by Abra Geiger 4/24/23
+"""
+Created April 2023
+Last Edited on Mon May 22 2023
+@author: Abra Geiger abrageiger7
 
-#cordes PBFs
+Convolving PBFs with varying gaussians
+"""
+
+
+#imports
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 from scipy.integrate import trapz
 from scipy.interpolate import CubicSpline
 
+#import profiles from professor Cordes
 cordes_profs = np.load('widths_pbf_data.npy')
 
+#array of beta values used
 betaselect = np.array([3.1, 3.5, 3.667, 3.8, 3.9, 3.95, 3.975, 3.99, 3.995, 3.9975, 3.999, 3.99999])
 
+#array of widths used (pbf stretch factors)
 widths = np.concatenate((np.linspace(0.1, 1.0, 10), np.linspace(1.1, 42.0, 40)))
 
+#array of gaussian widths (phase bins)
 widths_gaussian = np.linspace(0.1, 250.0, 50)
 
+#gaussian parameters in phase bins and arbitrary intensity comparitive to data
 parameters = np.zeros((50, 3))
 parameters[:,0] = 0.3619 #general amplitude to be scaled
 parameters[:,1] = 1025.0 #general mean
 parameters[:,2] = widths_gaussian #independent variable
 
 #phase bins
-t = np.linspace(0, 2048, 2048)
+phase_bins = 2048
+t = np.linspace(0, phase_bins, phase_bins)
 
 # first want to scale the time to match the phase bins
 #this way we have 9549 values and they go up to 2048s
-times_scaled = np.zeros(9549)
-for i in range(9549):
-    times_scaled[i] = 2048/9549*i
+cordes_phase_bins = 9549
+times_scaled = np.zeros(cordes_phase_bins)
+for i in range(cordes_phase_bins):
+    times_scaled[i] = phase_bins/cordes_phase_bins*i
 
+#an array of the broadening functions scaled to 2048 data values
 pbf_data_freqscale = np.zeros((np.size(betaselect), np.size(widths), 2048))
 
 data_index1 = 0
 for i in cordes_profs:
     data_index2 = 0
-    timetofreq_pbfdata = np.zeros((np.size(widths), 2048))
+    timetofreq_pbfdata = np.zeros((np.size(widths), phase_bins))
     for ii in i:
         interpolate_first_beta = CubicSpline(times_scaled, ii)
-        pbfdata_freqs = interpolate_first_beta(np.arange(0,2048,1))
+        pbfdata_freqs = interpolate_first_beta(np.arange(0,phase_bins,1))
         timetofreq_pbfdata[data_index2] = pbfdata_freqs
         data_index2 = data_index2+1
     pbf_data_freqscale[data_index1] = timetofreq_pbfdata
     data_index1 = data_index1+1
 
 #next we want to convert the broadening functions to unit area for convolution
-pbf_data_unitarea = np.zeros((np.size(betaselect), np.size(widths), 2048))
+pbf_data_unitarea = np.zeros((np.size(betaselect), np.size(widths), phase_bins))
 
 data_index1 = 0
 for i in pbf_data_freqscale:
@@ -57,8 +70,8 @@ for i in pbf_data_freqscale:
         data_index2 = data_index2+1
     data_index1 = data_index1+1
 
-#convolve the pbfs with varying gaussians for the final back of profiles to fit
-convolved_profiles = np.zeros((np.size(betaselect), np.size(widths), np.size(parameters[:,0]), 2048))
+#now convolve the pbfs with varying gaussians for the final bank of profiles to fit
+convolved_profiles = np.zeros((np.size(betaselect), np.size(widths), np.size(parameters[:,0]), phase_bins))
 #indicies of beta, template width, gaussian width, profile data
 
 data_index0 = 0
@@ -76,7 +89,9 @@ for i in pbf_data_unitarea:
         data_index1 = data_index1+1
     data_index0 = data_index0+1
 
-time = np.arange(0,2048,1) * (0.0021499/2048) * 1e3 #milliseconds
+sec_pulse_per = 0.0021499
+s_to_ms_conv = 1e3
+time = np.arange(0,phase_bins,1) * (sec_pulse_per/phase_bins) * s_to_ms_conv #milliseconds
 
 np.save('convolved_profs', convolved_profiles)
 
