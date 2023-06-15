e0bac616def98745ed1a2cc9f524bbf3244afcf9
Abra Geiger <abra.geiger@yahoo.com>
2023-06-15 17:14:34 -0400

    fixed typo related to dimension of arrays working with

--- debugging_and_testing.py
+++ debugging_and_testing.py
@@ -56,12 +56,12 @@ import tau
 
 for i in range(len(betaselect)):
     for ii in range(np.size(tau.tau_values)-1):
-        if tau.tau_values[ii] == tau.tau_values[ii+1]:
+        if tau.tau_values[i][ii] == tau.tau_values[i][ii+1]:
             print('MORE RESOLUTION NECESSARY')
 for ii in range(np.size(tau.tau_values_exp)-1):
     if tau.tau_values_exp[ii] == tau.tau_values_exp[ii+1]:
         print('MORE RESOLUTION NECESSARY')
 for i in range(len(zetaselect)):
     for ii in range(np.size(tau.zeta_tau_values)-1):
-        if tau.zeta_tau_values[ii] == tau.zeta_tau_values[ii+1]:
+        if tau.zeta_tau_values[i][ii] == tau.zeta_tau_values[i][ii+1]:
             print('MORE RESOLUTION NECESSARY')
