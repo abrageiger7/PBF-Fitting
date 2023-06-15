22db3fc742d12a0f56e36c9d3889f4b10d65db3b
Abra Geiger <abra.geiger@yahoo.com>
2023-06-15 17:12:19 -0400

    removed a typo

--- debugging_and_testing.py
+++ debugging_and_testing.py
@@ -57,11 +57,11 @@ import tau
 for i in range(len(betaselect)):
     for ii in range(np.size(tau.tau_values)-1):
         if tau.tau_values[ii] == tau.tau_values[ii+1]:
-            return('MORE RESOLUTION NECESSARY')
+            print('MORE RESOLUTION NECESSARY')
 for ii in range(np.size(tau.tau_values_exp)-1):
     if tau.tau_values_exp[ii] == tau.tau_values_exp[ii+1]:
-        return('MORE RESOLUTION NECESSARY')
+        print('MORE RESOLUTION NECESSARY')
 for i in range(len(zetaselect)):
     for ii in range(np.size(tau.zeta_tau_values)-1):
         if tau.zeta_tau_values[ii] == tau.zeta_tau_values[ii+1]:
-            return('MORE RESOLUTION NECESSARY')
+            print('MORE RESOLUTION NECESSARY')
