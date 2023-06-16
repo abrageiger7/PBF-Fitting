77a0bfdc568f7b4692fd7daf82ee2371ebecd5f6
Abra Geiger <abra.geiger@yahoo.com>
2023-06-16 10:41:29 -0400

    testing exponential at larger gwidth, 3 diff freqs

--- debugging_and_testing.py
+++ debugging_and_testing.py
@@ -40,6 +40,8 @@ freq0 = freq[0][:num_chan0]
 p = Profile(mjds[0], data0, freq0, dur[0])
 
 ii = 0
+iii = 11
+iv = 6
 
 datab = p.fit(ii, beta_ind = 11, gwidth_ind = 4)
 
@@ -49,19 +51,24 @@ print('low_chi, tau_fin, tau_low, tau_up, self.comp_fse(tau_fin), gwidth, pbf_wi
 
 print(datab)
 
+dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 6)
 
-import tau
+dataem = p.fit(iv, dec_exp=True, gwidth_ind = 6)
 
-#testing that no tau values are the same to confirm that grid spacing is resolved enough
+datael = p.fit(iii, dec_exp=True, gwidth_ind = 6)
 
-for i in range(len(betaselect)):
-    for ii in range(np.size(tau.tau_values[0])-1):
-        if tau.tau_values[i][ii] == tau.tau_values[i][ii+1]:
-            print('MORE RESOLUTION NECESSARY')
-for ii in range(np.size(tau.tau_values_exp)-1):
-    if tau.tau_values_exp[ii] == tau.tau_values_exp[ii+1]:
-        print('MORE RESOLUTION NECESSARY')
-for i in range(len(zetaselect)):
-    for ii in range(np.size(tau.zeta_tau_values[0])-1):
-        if tau.zeta_tau_values[i][ii] == tau.zeta_tau_values[i][ii+1]:
-            print('MORE RESOLUTION NECESSARY')
+
+
+# import tau
+# #testing that no tau values are the same to confirm that grid spacing is resolved enough
+# for i in range(len(betaselect)):
+#     for ii in range(np.size(tau.tau_values[0])-1):
+#         if tau.tau_values[i][ii] == tau.tau_values[i][ii+1]:
+#             print('MORE RESOLUTION NECESSARY')
+# for ii in range(np.size(tau.tau_values_exp)-1):
+#     if tau.tau_values_exp[ii] == tau.tau_values_exp[ii+1]:
+#         print('MORE RESOLUTION NECESSARY')
+# for i in range(len(zetaselect)):
+#     for ii in range(np.size(tau.zeta_tau_values[0])-1):
+#         if tau.zeta_tau_values[i][ii] == tau.zeta_tau_values[i][ii+1]:
+#             print('MORE RESOLUTION NECESSARY')
