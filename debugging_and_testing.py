3da86debf472430c95c7b95d235782fabbd565f1
Abra Geiger <abra.geiger@yahoo.com>
2023-06-19 11:09:19 -0400

    testing to see fav gaussian width for all highest freqs

--- debugging_and_testing.py
+++ debugging_and_testing.py
@@ -32,140 +32,128 @@ mjds = np.load("J1903_mjds.npy")
 chan = np.load("J1903_numchan.npy")
 dur = np.load("J1903_dur.npy")
 
-num_chan0 = int(chan[0])
-data0 = data[0][:num_chan0]
-freq0 = freq[0][:num_chan0]
-
-
-p = Profile(mjds[0], data0, freq0, dur[0])
-
 ii = 0
-iii = 11
-iv = 6
 
-datab = p.fit(ii, beta_ind = 11, gwidth_ind = 4)
+high_freq_gwidth_test = np.zeros((56,2))
 
-print(mjds[0])
+for i in range(56):
 
-print('low_chi, tau_fin, tau_low, tau_up, self.comp_fse(tau_fin), gwidth, pbf_width_fin)')
+    num_chan = int(chan[i])
+    data = data[i][:num_chan]
+    freq = freq[i][:num_chan]
 
-print(datab)
+    p = Profile(mjds[i], data, freq, dur[i])
 
-dataeh = p.fit(ii, dec_exp=True)
-print(dataeh)
+    dataret = p.fit(ii, dec_exp=True)
 
-dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 3)
-print(dataeh[0])
+    high_freq_gwidth_test[i][0] = p.freq_suba #frequency
+    high_freq_gwidth_test[i][1] = dataret[5] #gaussian width
 
-dataem = p.fit(iv, dec_exp=True, gwidth_ind = 3)
-print(dataem[0])
+    print(f'Frequency = {p.freq_round} MHz')
+    print(fr'Gaussian Width = {dataret[5]} $\mu$s')
 
-datael = p.fit(iii, dec_exp=True, gwidth_ind = 3)
-print(datael[0])
-
-dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 4)
-print(dataeh[0])
-
-dataem = p.fit(iv, dec_exp=True, gwidth_ind = 4)
-print(dataem[0])
-
-datael = p.fit(iii, dec_exp=True, gwidth_ind = 4)
-print(datael[0])
-
-dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 5)
-print(dataeh[0])
-
-dataem = p.fit(iv, dec_exp=True, gwidth_ind = 5)
-print(dataem[0])
-
-datael = p.fit(iii, dec_exp=True, gwidth_ind = 5)
-print(datael[0])
-
-dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 6)
-print(dataeh[0])
-
-dataem = p.fit(iv, dec_exp=True, gwidth_ind = 6)
-print(dataem[0])
-
-datael = p.fit(iii, dec_exp=True, gwidth_ind = 6)
-print(datael[0])
+np.save('high_freq_gwidth_test', high_freq_gwidth_test)
 
 
-dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 7)
-print(dataeh[0])
+#===============================================================================
+# Printing lowest chi-squared values to get a sense of the best gaussian width
+# ==============================================================================
 
+# dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 3)
+# print(dataeh[0])
+#
+# dataem = p.fit(iv, dec_exp=True, gwidth_ind = 3)
+# print(dataem[0])
+#
+# datael = p.fit(iii, dec_exp=True, gwidth_ind = 3)
+# print(datael[0])
+#
+# dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 4)
+# print(dataeh[0])
+#
+# dataem = p.fit(iv, dec_exp=True, gwidth_ind = 4)
+# print(dataem[0])
+#
+# datael = p.fit(iii, dec_exp=True, gwidth_ind = 4)
+# print(datael[0])
+#
+# dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 5)
+# print(dataeh[0])
+#
+# dataem = p.fit(iv, dec_exp=True, gwidth_ind = 5)
+# print(dataem[0])
+#
+# datael = p.fit(iii, dec_exp=True, gwidth_ind = 5)
+# print(datael[0])
+#
+# dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 6)
+# print(dataeh[0])
+#
+# dataem = p.fit(iv, dec_exp=True, gwidth_ind = 6)
+# print(dataem[0])
+#
+# datael = p.fit(iii, dec_exp=True, gwidth_ind = 6)
+# print(datael[0])
+#
+# dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 7)
+# print(dataeh[0])
+#
+# dataem = p.fit(iv, dec_exp=True, gwidth_ind = 7)
+# print(dataem[0])
+#
+# datael = p.fit(iii, dec_exp=True, gwidth_ind = 7)
+# print(datael[0])
+#
+# dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 8)
+# print(dataeh[0])
+#
+# dataem = p.fit(iv, dec_exp=True, gwidth_ind = 8)
+# print(dataem[0])
+#
+# datael = p.fit(iii, dec_exp=True, gwidth_ind = 8)
+# print(datael[0])
+#
+# dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 9)
+# print(dataeh[0])
+#
+# dataem = p.fit(iv, dec_exp=True, gwidth_ind = 9)
+# print(dataem[0])
+#
+# datael = p.fit(iii, dec_exp=True, gwidth_ind = 9)
+# print(datael[0])
+#
+# dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 10)
+# print(dataeh[0])
+#
+# dataem = p.fit(iv, dec_exp=True, gwidth_ind = 10)
+# print(dataem[0])
+#
+# datael = p.fit(iii, dec_exp=True, gwidth_ind = 10)
+# print(datael[0])
+#
+# dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 11)
+# print(dataeh[0])
+#
+# dataem = p.fit(iv, dec_exp=True, gwidth_ind = 11)
+# print(dataem[0])
+#
+# datael = p.fit(iii, dec_exp=True, gwidth_ind = 11)
+# print(datael[0])
+#
+# dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 12)
+# print(dataeh[0])
+#
+# dataem = p.fit(iv, dec_exp=True, gwidth_ind = 12)
+# print(dataem[0])
+#
+# datael = p.fit(iii, dec_exp=True, gwidth_ind = 12)
+# print(datael[0])
 
-dataem = p.fit(iv, dec_exp=True, gwidth_ind = 7)
-print(dataem[0])
-
-
-datael = p.fit(iii, dec_exp=True, gwidth_ind = 7)
-print(datael[0])
-
-
-dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 8)
-print(dataeh[0])
-
-
-dataem = p.fit(iv, dec_exp=True, gwidth_ind = 8)
-print(dataem[0])
-
-
-datael = p.fit(iii, dec_exp=True, gwidth_ind = 8)
-print(datael[0])
-
-
-dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 9)
-print(dataeh[0])
-
-
-dataem = p.fit(iv, dec_exp=True, gwidth_ind = 9)
-print(dataem[0])
-
-
-datael = p.fit(iii, dec_exp=True, gwidth_ind = 9)
-print(datael[0])
-
-
-dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 10)
-print(dataeh[0])
-
-
-dataem = p.fit(iv, dec_exp=True, gwidth_ind = 10)
-print(dataem[0])
-
-
-datael = p.fit(iii, dec_exp=True, gwidth_ind = 10)
-print(datael[0])
-
-
-dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 11)
-print(dataeh[0])
-
-
-dataem = p.fit(iv, dec_exp=True, gwidth_ind = 11)
-print(dataem[0])
-
-
-datael = p.fit(iii, dec_exp=True, gwidth_ind = 11)
-print(datael[0])
-
-
-dataeh = p.fit(ii, dec_exp=True, gwidth_ind = 12)
-print(dataeh[0])
-
-
-dataem = p.fit(iv, dec_exp=True, gwidth_ind = 12)
-print(dataem[0])
-
-
-datael = p.fit(iii, dec_exp=True, gwidth_ind = 12)
-print(datael[0])
-
-
+#===============================================================================
+# testing that no tau values are the same to confirm that grid spacing is resolved enough
+# ==============================================================================
 
 # import tau
-# #testing that no tau values are the same to confirm that grid spacing is resolved enough
 # for i in range(len(betaselect)):
 #     for ii in range(np.size(tau.tau_values[0])-1):
 #         if tau.tau_values[i][ii] == tau.tau_values[i][ii+1]:
