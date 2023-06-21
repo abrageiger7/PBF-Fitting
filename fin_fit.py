9e016827f8a3f59b95df253aaf8a48af7e06b23f
Abra Geiger <abra.geiger@yahoo.com>
2023-06-21 00:59:27 -0400

    changed back to 55 lags

--- fin_fit.py
+++ fin_fit.py
@@ -663,10 +663,10 @@ def power_laws_and_plots(beta_ind, beta_gwidth_ind):
     plt.rc('xtick', labelsize=12)
     plt.rc('ytick', labelsize=10)
 
-    acor0 = axs.flat[0].acorr(plaw_datae[:,0], label = r'Exponential PBF $\alpha$')
-    acor1 = axs.flat[1].acorr(plaw_datae[:,3], label = r'Exponential PBF $\tau_0$')
-    acor2 = axs.flat[2].acorr(plaw_datab[:,0], label = r'Beta = 3.99999 PBF $\alpha$')
-    acor3 = axs.flat[3].acorr(plaw_datab[:,3], label = r'Beta = 3.99999 PBF $\tau_0$')
+    acor0 = axs.flat[0].acorr(plaw_datae[:,0], label = r'Exponential PBF $\alpha$', maxlags = 55)
+    acor1 = axs.flat[1].acorr(plaw_datae[:,3], label = r'Exponential PBF $\tau_0$', maxlags = 55)
+    acor2 = axs.flat[2].acorr(plaw_datab[:,0], label = r'Beta = 3.99999 PBF $\alpha$', maxlags = 55)
+    acor3 = axs.flat[3].acorr(plaw_datab[:,3], label = r'Beta = 3.99999 PBF $\tau_0$', maxlags = 55)
 
     fig.tight_layout()
     plt.savefig('timea_autocorrelation.pdf')
